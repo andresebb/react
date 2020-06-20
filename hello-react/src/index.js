9045a1c8b5921f5e5be768a6e91b2d4075316c3e
andres <aebb001@gmail.com>
2020-06-20 09:50:29 -0400

    Diferencia entre jsx y React.createElement.

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -10,12 +10,40 @@ container.appendChild(element);
 
 //Esta es la forma de hacerlo con react
 
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges soy react!</h1>;
+const name = 'Eduardo';
+
+//Aca trabajamos con jsx y react.createElement este ultimo no se utiliza casi;
+
+/* const element = React.createElement('a', {href: 'https://platzi.com'}, 'Ir a platzi' ); */
+/* const element = React.createElement('h1', {}, `Hola soy ${name}`); */
+/* const element = React.createElement(
+    'div',
+    {},
+    React.createElement('h1', {}, 'Hola soy andre'),
+    React.createElement('p', {}, 'Esta es el parrado de reactcreateelemet'),
+);
+
+
+
+/* const jsx = <h1>Hello, Platzi Badges soy react!</h1>; */
+/* const jsx = <h1>Hola soy, {name}</h1>; */
+
+const jsx = (
+    <div>
+        <h1>Hola soy andres</h1>
+        <p>Asi insertamos codigo a traves de jsx</p>
+    </div>
+);
+
+
+
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
