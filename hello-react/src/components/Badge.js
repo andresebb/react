48e8ce5072a911f7ff2e3febbcd3e684abd29b36
andres <aebb001@gmail.com>
2020-06-20 11:52:17 -0400

    Aplicando estilos / instalamos bootstrap

--- hello-react/src/components/Badge.js
+++ hello-react/src/components/Badge.js
@@ -2,33 +2,39 @@ import React from 'react';
 
 import confLogo from '../images/badge-header.svg';
 
-class Badgee extends React.Component{
+import "./styles/Badge.css";
+
+
+class Badge extends React.Component{
     
     /*Todos los componenten necesitan por lo menos un metodo,
     y ese es el render, que define el resultado que veremos
     en pantalla*/
 
+     /* En react no se usa class, se usa className en el html */
+
     render() {
         return(
-            <div>
-                <div>
+            <div className="Badge"> 
+                <div className="Badge__header">
                     <img src={confLogo}></img>
                 </div>
 
-                <div>
-                    <img src="https://www.gravatar.com/avatar?d=identicon" alt="avatar"></img>
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src="https://s.gravatar.com/avatar/484479aae5659fd266b2864a69b7c7cd?s=80
+" alt="avatar"></img>
                     <h1>Andres <br/> Betancourt</h1>
                 </div>
 
-                <div>
-                    <p>Frontend Developer</p>
-                    <p>@andresebb</p>
+                <div className="Badge__section-info">
+                    <h3>Frontend Developer</h3>
+                    <div>@andresebbe</div>
                 </div>
 
-                <div>#platziconf</div>
+                <div className="Badge__footer">#platziconf</div>
             </div>
         )
     }
 }
 
-export default Badgee; //Aqui estamos exportando es la clase, no el archivo
+export default Badge; //Aqui estamos exportando es la clase, no el archivo
--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Badgee from './components/Badge';
+import Badge from './components/Badge';
+import 'bootstrap/dist/css/bootstrap.css'; //Como es un archivo de npm se le pasa asi
+
+import './global.css';
 
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<Badgee />, container);
+ReactDOM.render(<Badge />, container);
