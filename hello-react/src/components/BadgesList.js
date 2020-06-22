63c6f3dba9978ac64ded95bdb4a4ea1e29918751
andres <aebb001@gmail.com>
2020-06-22 12:18:16 -0400

    lista de componentes (Agregamos la pagina badges y adento le colocamos BadgesList)

---
+++ hello-react/src/components/BadgesList.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import './styles/BadgeList.css'
+
+import avatar from '../images/logo.svg'
+
+import twitterLogo from '../images/twitterLogo.svg'
+
+class BadgesList extends React.Component{
+    render(){
+        return(
+            <div className="BadgesList">
+                <ul className="list-unstyled">
+                    {/* vamos a mapear cada uno de los objetos que estan en state (en badges.js)y los
+                    metemos dentro de la variable badge */}
+                    {this.props.badges.map((badge) => {
+                        return(
+                            <li key={badge.id}>
+                                <div className="image">
+                                    <img className="" src={badge.avatarUrl}></img>
+                                </div>
+
+                                <div className="about">
+                                    <div className="about__name">
+                                        <p>{badge.firstName} {badge.lastName}</p>
+                                    </div>
+                                    <div className="about__job">
+                                        <p>{badge.jobTitle}</p>
+                                    </div>
+                                    <div className="about__twitter">
+                                        <div className="twitter__logo">
+                                        <img src={twitterLogo}></img>
+                                        </div>
+                                        <p>@{badge.twitter}</p>
+                                    </div>
+                                </div>
+
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default BadgesList;
--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import BadgeNew from './pages/BadgeNew';
 import 'bootstrap/dist/css/bootstrap.css'; //Como es un archivo de npm se le pasa asi
-
 import './global.css';
+
+
+import BadgeNew from './pages/BadgeNew';
+import Badges from './pages/Badges';
+
 
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<BadgeNew />, container);
+ReactDOM.render(<Badges />, container);
