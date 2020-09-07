37737c8a642c02ec60f2056c3b59cff552e6b5b1
andres <aebb001@gmail.com>
2020-09-07 10:28:04 -0400

    Updates

---
+++ precache-manifest.31a6242492da992f5b00297628b5ef32.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579b5333634a797337897cdf96be75ca",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "6ab89c2169b9b61196b9",
+    "url": "/react/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "46c8d100babbaf365498",
+    "url": "/react/static/css/main.c9213eac.chunk.css"
+  },
+  {
+    "revision": "6ab89c2169b9b61196b9",
+    "url": "/react/static/js/2.8037908e.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/react/static/js/2.8037908e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c8d100babbaf365498",
+    "url": "/react/static/js/main.af4df39b.chunk.js"
+  },
+  {
+    "revision": "09937f70ecfd4e48b40b",
+    "url": "/react/static/js/runtime-main.5e819cf2.js"
+  },
+  {
+    "revision": "0d2ff286deb3895d3ee82fed78c4dce4",
+    "url": "/react/static/media/astronauts.0d2ff286.svg"
+  },
+  {
+    "revision": "ed7c58eafe17eff3b2557c0369b20264",
+    "url": "/react/static/media/badge-header.ed7c58ea.svg"
+  },
+  {
+    "revision": "6841730f1bbe3235e7a0cdc7d244d244",
+    "url": "/react/static/media/logo.6841730f.svg"
+  },
+  {
+    "revision": "a1880fbfb5634577b88090c9f82b3de7",
+    "url": "/react/static/media/platziconf-logo.a1880fbf.svg"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/react/static/media/stars.0abcdfcd.svg"
+  }
+]);
